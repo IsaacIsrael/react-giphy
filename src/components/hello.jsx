ab537483c007706e0f7c4f1a590b377925a6bc06
isaac israel <email.iisrael@gmail.com>
2019-04-11 11:58:36 -0300

    Create A Gif search

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-import App from './components/app';
+const Hello = ({ name }) => {
+  return (
+    <div>
+      Hello,
+      {name}
+    </div>
+  );
+};
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(
-    <App />,
-    root
-  );
+  ReactDOM.render(<Hello name="World" />, root);
 }
